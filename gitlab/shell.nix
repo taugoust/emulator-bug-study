db3854222fcaed8ff849267f10348c7d971fa325
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2026-03-17 13:30:27 +0100

    Remove old scripts

--- gitlab/shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
-with pkgs;
-mkShell {
-  buildInputs = [
-    python312Packages.tomlkit
-    python312Packages.requests
-  ];
-}
--- mailinglist/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
-with pkgs;
-mkShell {
-  buildInputs = [
-    python312Packages.html5lib
-    python312Packages.beautifulsoup4
-    python312Packages.requests
-  ];
-}
