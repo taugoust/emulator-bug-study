c66022ee6cdc35b0ed85e9d2d447452a7b24cda1
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2026-03-18 11:19:57 +0100

    Remove exit() on TypeError in output_issue()

--- tools/scrape/scrape/github.py
+++ tools/scrape/scrape/github.py
@@ -8,14 +8,10 @@ from buglib import (
 
 
 def output_issue(issue: dict, output_dir: str = "issues") -> None:
-    try:
-        if 'documentation' in issue['labels']:
-            write_file(f"{output_dir}/documentation/{issue['id']}", issue['title'] + '\n' + (issue['description'] or ""))
-        else:
-            write_file(f"{output_dir}/{issue['id']}", issue['title'] + '\n' + (issue['description'] or ""))
-    except TypeError:
-        print(f"error with bug {issue['id']}")
-        exit()
+    if 'documentation' in issue['labels']:
+        write_file(f"{output_dir}/documentation/{issue['id']}", issue['title'] + '\n' + (issue['description'] or ""))
+    else:
+        write_file(f"{output_dir}/{issue['id']}", issue['title'] + '\n' + (issue['description'] or ""))
 
 
 def _parse_issue(i: dict) -> dict | None:
